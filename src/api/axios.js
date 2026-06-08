862451aaed43bdb2c9da7cbd5db91446e6498d5c
Aniq Abbas <aniqabbas5@gmail.com>
2026-06-08 20:37:00 +0800

    Update frontend API backend URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://web-production-2db875.up.railway.app/api",
+  baseURL: "https://omasync-backend-production.up.railway.app/api",
 });
 
 export default api;
